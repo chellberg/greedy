46b5c2dd5732ec7dc3cd35b5c288578b07ba0717
Corin Chellberg <corinchellberg@gmail.com>
2016-09-15 22:27:41 -0400

    add overlap_length method, test

--- greedy.rb
+++ greedy.rb
@@ -1,10 +1,37 @@
 class Greedy
-  def initialize filename
+  def initialize filename, minimum_overlap: 450
     @filename = filename
+    @minimum_overlap = minimum_overlap
+  end
+
+  string = 'foobar'
+  overlap_candidate = 'barbaz'
+
+
+  def overlap_length string, overlap_candidate
+    # index at which to begin looking in string for the overlap_candidate substring
+    match_index = 0
+    while true
+      # search string for the first minimum_overlap length substring of overlap_candidate
+      match_index = string.index overlap_candidate[0..@minimum_overlap], match_index
+      return nil if !match_index
+
+      # check that the overlap_candidate starts with the remaining substring of a from the match index onward
+      if overlap_candidate.start_with? string[match_index..-1]
+        overlap_length = string.length - match_index
+        return overlap_length
+      end
+
+      # substring match found in string, but the rest of string doesn't match the beginning of overlap_candidate
+      match_index += 1
+    end
   end
 
   def reads
     # drop 1 because the first element will be ""
-    @reads ||= File.read(@filename).delete("\r\n").split(/>Rosalind_\d{4}/).drop(1)
+    @reads ||= File.read(@filename)
+                   .delete("\r\n")
+                   .split(/>Rosalind_\d{4}/)
+                   .drop(1)
   end
 end
--- spec.rb
+++ spec.rb
@@ -3,12 +3,45 @@ require 'minitest/autorun'
 
 describe Greedy do
   before do
-    @greedy = Greedy.new 'sample_data.txt'
+    @greedy = Greedy.new 'sample_data.txt', minimum_overlap: 3
   end
 
-  it 'reads inputs correctly' do
-    expected = %w(ATTAGACCTG CCTGCCGGAA AGACCTGCCG GCCGGAATAC)
-    assert_equal @greedy.reads, expected
+  describe '#reads' do
+    it 'reads inputs correctly' do
+      expected = %w(ATTAGACCTG CCTGCCGGAA AGACCTGCCG GCCGGAATAC)
+      assert_equal expected, @greedy.reads
+    end
+  end
+
+  describe '#overlap_length' do
+    describe 'when the strings overlap at the first substring match' do
+      it 'returns the position of the overlap' do
+        string = 'foobarbaz'
+        overlap_candidate = 'barbazfizz' # starting_index will be 3
+        overlap_length = @greedy.overlap_length string, overlap_candidate
+
+        assert_equal 6, overlap_length
+      end
+    end
+
+    describe 'when the strings overlap at the second substring match' do
+      it 'returns the position of the overlap' do
+        string = 'barbizbarflies'
+        overlap_candidate = 'barfliesrule'
+        overlap_length = @greedy.overlap_length string, overlap_candidate
+
+        assert_equal 8, overlap_length
+      end
+    end
+
+    describe 'when there is no substring match' do
+      it 'returns nil' do
+        string = 'foobarbaz'
+        overlap_candidate = 'fizzbizfudge'
+        overlap_length = @greedy.overlap_length string, overlap_candidate
+
+        assert overlap_length.nil?
+      end
+    end
   end
 end
-
