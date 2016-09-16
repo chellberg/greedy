497dfc89c5c01e438a92a6a1e12502e4b96eac10
Corin Chellberg <corinchellberg@gmail.com>
2016-09-15 21:39:20 -0400

    read data, test

--- greedy.rb
+++ greedy.rb
@@ -1,3 +1,10 @@
 class Greedy
+  def initialize filename
+    @filename = filename
+  end
 
+  def reads
+    # drop 1 because the first element will be ""
+    @reads ||= File.read(@filename).delete("\r\n").split(/>Rosalind_\d{4}/).drop(1)
+  end
 end
--- spec.rb
+++ spec.rb
@@ -1,5 +1,14 @@
+require_relative 'greedy'
 require 'minitest/autorun'
 
 describe Greedy do
+  before do
+    @greedy = Greedy.new 'sample_data.txt'
+  end
 
+  it 'reads inputs correctly' do
+    expected = %w(ATTAGACCTG CCTGCCGGAA AGACCTGCCG GCCGGAATAC)
+    assert_equal @greedy.reads, expected
+  end
 end
+
